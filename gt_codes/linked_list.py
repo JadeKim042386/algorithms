4033f9366f1ba4318cc5743ae21f12031626cec5
JadeKim042386 <jk042386@gmail.com>
2023-05-16 13:16:01 +0900

    add linked list

---
+++ gt_codes/linked_list.py
@@ -0,0 +1,103 @@
+class Node:
+    def __init__(self, data) -> None:
+        self.data = data
+        self.next = None
+
+    def __repr__(self) -> str:
+        return f"Node({self.data})"
+
+
+class LinkedList:
+    def __init__(self) -> None:
+        self.head = None
+
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node.data
+            node = node.next
+    
+    def __len__(self):
+        return sum(1 for _ in self)
+    
+    def __repr__(self) -> str:
+        return "->".join(str(item) for item in self)
+    
+    def __getitem__(self, index):
+        if not 0 <= index < len(self):
+            raise ValueError("list index out of range.")
+        for i, node in enumerate(self):
+            if i == index:
+                return node
+        return None
+    
+    def __setitem__(self, index, data):
+        if not 0 <= index < len(self):
+            raise ValueError("list index out of range.")
+        current = self.head
+        for _ in range(index):
+            current = current.next
+        current.data = data
+    
+    def insert(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            temp = self.head
+            for _ in range(len(self) - 1):
+                temp = temp.next
+            temp.next = new_node
+
+    def insert_nth(self, index, data):
+        if not 0 <= index < len(self):
+            raise ValueError("list index out of range.")
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        elif index == 0:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            temp = self.head
+            for _ in range(index - 1):
+                temp = temp.next
+            new_node.next = temp.next
+            temp.next = new_node
+    
+    def print_list(self):
+        print(self)
+
+    def delete_nth(self, index):
+        if not 0 <= index < len(self) - 1:
+            raise ValueError("list index out of range.")
+        delete_node = self.head
+        if index == 0:
+            self.head = self.head.next
+        else:
+            temp = self.head
+            for _ in range(index - 1):
+                temp = temp.next
+            delete_node = temp.next
+            temp.next = temp.next.next
+        return delete_node.data
+    
+    def reverse(self):
+        prev = None
+        current = self.head
+        while current:
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
+        self.head = prev
+
+if __name__ == '__main__':
+    linked_list = LinkedList()
+    linked_list.insert(0)
+    linked_list.insert(1)
+    linked_list.insert(2)
+    linked_list.insert_nth(1, 3)
+    linked_list.print_list()
+    linked_list.reverse() # https://media.geeksforgeeks.org/wp-content/cdn-uploads/RGIF2.gif
+    linked_list.print_list()
